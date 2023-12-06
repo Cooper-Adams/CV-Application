0320d7cfd20ecda185e0eee8284066e7a38144dd
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-06 00:35:32 -0500

    Updated the app to contain the personal and education information input sections

--- src/CV_App.jsx
+++ src/CV_App.jsx
@@ -1,15 +1,45 @@
 import React, { useState } from 'react'
-import './CV_App.css'
+import Personal_Info from './components/Personal_Info'
+import Education_Info from './components/Education_Info'
+import Work_Info from './components/Work_Info'
+import './styles/CV_App.css'
 
-const CV_App = (props) => {
-  const [personalForm, setPersonalForm] = useState('')
+function CV_App () {
+  const [personalForm, setPersonalForm] = useState([])
   const [educationForm, setEduForm] = useState([])
   const [professionalForm, setProfessionalForm] = useState([])
 
+  const saveFormInput = (formData) => {
+    switch (formData.form) {
+      case 'personal-form':
+        setPersonalForm(formData)
+        break
+      case 'education-form':
+        //setEducationForm(formData)
+        break
+      case 'professional-form':
+        //setProfessionalForm(formData)
+        break
+    }
+  }
+
+  const toggleCollapsed = (e) => toggleValue(e, "isCollapsed")
+  const toggleHidden = (e) => toggleValue(e, "isHidden")
+
   return (
     <>
-      <div>
-        <h1>123 testing</h1>
+      <div className="cv-input">
+        {/* <Modify_Form /> */}
+        <Personal_Info
+          saveFormInput={saveFormInput}
+          onChange={setPersonalForm}
+        />
+        <Education_Info />
+        {/* <Work_Info /> */}
+      </div>
+
+      <div className="cv-preview">
+        {/* <Live_Preview /> */}
       </div>
     </>
   )
