0d4cec57a47f4b304e06b5681a96974649f2c323
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-11 18:46:10 -0500

    Added the basic layout of the CV preview

--- src/components/CV_Preview.jsx
+++ src/components/CV_Preview.jsx
@@ -1,1 +1,25 @@
 import React, { useState } from 'react'
+
+const Live_Preview = (props) => {
+    <>
+        <div className="cv-section personal">
+            <div className="personal-info">
+
+            </div>
+
+            <div className="personal-contact">
+
+            </div>
+        </div>
+
+        <div className="cv-section education">
+
+        </div>
+
+        <div className="cv-section professional">
+
+        </div>
+    </>
+}
+
+export default Live_Preview
