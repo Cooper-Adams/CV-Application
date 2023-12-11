51da02e7adafd782f3fc7d4cc2a86b18ae733f6e
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-11 18:46:45 -0500

    Added a button to download the CV

--- src/components/Form_Modification.jsx
+++ src/components/Form_Modification.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Form_Modification = (props) => {
   const clearForm = () => { props.wipeFormData() }
+  const loadForm = () => { props.exampleFormData() }
+  const downloadCV = () => { props.generatePDF() }
 
   return (
     <>
       <div className='info-div'>
         <div className='info-head'>
-          <div className='modification-btn'><span>Load Example</span></div>
           <div className='modification-btn' onClick={clearForm}><span>Clear CV</span></div>
+          <div className='modification-btn' onClick={loadForm}><span>Load Example</span></div>
+          <div className='modification-btn' onClick={downloadCV}><span>Download</span></div>
         </div>
       </div>
     </>
