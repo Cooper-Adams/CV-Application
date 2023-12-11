601509267db33ac33d945153af5f505be468befd
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-11 18:45:46 -0500

    Added the bones of the pdf generation function and added the live preview function

--- src/CV_App.jsx
+++ src/CV_App.jsx
@@ -3,6 +3,7 @@ import Personal_Info from './components/Personal_Info'
 import Education_Info from './components/Education_Info'
 import Work_Info from './components/Work_Info'
 import Form_Modification from './components/Form_Modification'
+import Live_Preview from './components/CV_Preview'
 import './styles/CV_App.css'
 
 function CV_App () {
@@ -10,14 +11,18 @@ function CV_App () {
   const [educationForm, setEducationForm] = useState([])
   const [professionalForm, setProfessionalForm] = useState([])
 
-  const exampleFormData = () => {
-    /* Insert logic to populate the CV display with example data */
-  }
-
   const wipeFormData = () => {
     setPersonalForm({firstName: '', lastName: '', email: '', phone: '', address: '', fileName: ''})
     setEducationForm({schoolName: '', degree: '', schoolStart: '', schoolFinish: ''})
     setProfessionalForm({companyName: '', jobTitle: '', startDate: '', finishDate: '', jobDescription: ''})
+  }
+
+  const exampleFormData = () => {
+    /* Insert logic to populate the CV display with example data */
+  }
+
+  const generatePDF = () => {
+    /* Insert logic to generate and download a PDF of the CV*/
   }
 
   const saveFormInput = (formData) => {
@@ -38,8 +43,9 @@ function CV_App () {
     <>
       <div className="cv-input">
         <Form_Modification
-          exampleFormData={exampleFormData}
           wipeFormData={wipeFormData}
+          exampleFormData={exampleFormData}
+          generatePDF={generatePDF}
         />
         <Personal_Info
           saveFormInput={saveFormInput}
@@ -53,7 +59,11 @@ function CV_App () {
       </div>
 
       <div className="cv-preview">
-        {/* <Live_Preview /> */}
+        <Live_Preview
+          personalForm={personalForm}
+          educationForm={educationForm}
+          professionalForm={professionalForm}
+        />
       </div>
     </>
   )
