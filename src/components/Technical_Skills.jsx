5531f8c716ff9628722258614a89223eb35d87b4
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-18 16:08:55 -0500

    Updated the functionality to add a title and then skills under that category, still needs to be fixed

--- src/components/Technical_Skills.jsx
+++ src/components/Technical_Skills.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react'
-import toggleForm from './Form_Toggle'
 
 const Technical_Skills = (props) => {
-    const [inputField, setInputField] = useState('')
+    const [inputFieldA, setInputFieldA] = useState('')
+    const [inputFieldB, setInputFieldB] = useState('')
     const [technicalSkills, setTechnicalSkills] = useState([])
+    const [skills, setSkills] = useState([])
     
     const handleChange = (e) => {
-        setInputField(e.target.value)
+        switch (e.target.name) {
+            case 'skillTitle':
+                setInputFieldA(e.target.value)
+                break
+            case 'skill':
+                setInputFieldB(e.target.value)
+                break
+        }
 
         e.preventDefault()
     }
 
-    const submitForm = (e) => { if (inputField.length) { handleFormSubmit(e.nativeEvent) } }
+    const submitForm = (e) => { if (inputFieldA.length && skills.length) { handleFormSubmit(e.nativeEvent) } }
 
     const handleFormSubmit = (e) => {
+        setSkills([
+            ...skills,
+            inputFieldB
+        ])
+
         setTechnicalSkills([
             ...technicalSkills,
-            {id: technicalSkills.length, name: inputField}
+            {id: technicalSkills.length, title: inputFieldA, skills: skills}
         ])
 
         props.saveSkillsArray(technicalSkills)
-        setInputField('')
+        setInputFieldA('')
+        setInputFieldB('')
     }
 
     return (
@@ -28,11 +42,11 @@ const Technical_Skills = (props) => {
             <div className='info-div'>
                 <div className='info-head'>
                     <h2 className='info-title'>Technical Skills</h2>
-                    <span className='info-toggle' onClick={toggleForm}></span>
                 </div>
 
                 <div className="info-section user-name">
-                    <input className='info-input' value={inputField} onChange={handleChange} name='inputField' type='text' placeholder='Technical Skill' />
+                    <input className='info-input' value={inputFieldA} onChange={handleChange} name='skillTitle' type='text' placeholder='Skill Title (Tools/Languages/etc.)' />
+                    <input className='info-input' value={inputFieldB} onChange={handleChange} name='skills' type='text' placeholder='Skill' />
                     <div className='update-cv' onClick={submitForm}><span>Add Skill</span></div>
                 </div>
 
@@ -44,7 +58,7 @@ const Technical_Skills = (props) => {
                                 <button onClick={() => {
                                     setTechnicalSkills(
                                         technicalSkills.filter(a =>
-                                        a.id !== skills.id
+                                            a.id !== skills.id
                                         )
                                     )
                                 }}>
