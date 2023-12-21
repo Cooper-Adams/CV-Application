263ed7bfe1c9729667239e3706f2c4fd259ddc8d
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-21 18:50:01 -0500

    Changed the personal rendering to take either the prop or a default value so the form always has content, be it the user's or example

--- src/components/Render_Personal.jsx
+++ src/components/Render_Personal.jsx
@@ -4,63 +4,54 @@ const Render_Personal = (props) => {
     return (
         <>
             <div className='cv-name-desc'>
-                {Object.entries(props.props).map(([key, value]) => {
-                    if (key == 'fullName') {
-                        return (
-                            <h1 key={key} className='cv-title-h1'>
-                                {props.props[key]}
-                            </h1>
-                        )
-                    } else if (key == 'title') {
-                        return (
-                            <h3 key={key} className='cv-title-h3'>
-                                {props.props[key]}
-                            </h3>
-                        )
-                    } else if (key == 'description') {
-                        return (
-                            <p key={key} className='cv-desc-p'>
-                                {props.props[key]}
-                            </p>
-                        )
-                    }
-                })}
+                <h1 className='cv-title-h1'>
+                    {props.props.fullName || 'Your Name'}
+                </h1>
+
+                <h3 className='cv-title-h3'>
+                    {props.props.title || 'Professional Title'}
+                </h3>
+
+                <p className='cv-desc-p'>
+                    {props.props.description || 'Description detailing your general skillset and abilities.'}
+                </p>
             </div>
 
             <div className='cv-contacts'>
-                {Object.entries(props.props).map(([key, value]) => {
-                    if (key == 'address') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {props.props[key]}
-                            </p>
-                        )
-                    } else if (key == 'email') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {props.props[key]}
-                            </p>
-                        )
-                    } else if (key == 'phone') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {props.props[key]}
-                            </p>
-                        )
-                    } else if (key == 'linkedin') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {props.props[key]}
-                            </p>
-                        )
-                    } else if (key == 'website') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {props.props[key]}
-                            </p>
-                        )
-                    }
-                })}
+                <div className="cv-contact-div">
+                    <p className='cv-contact-p'>
+                        {props.props.email || 'email@gmail.com'}
+                    </p>
+                </div>
+
+                <div className="cv-contact-div">
+                    <p className='cv-contact-p'>
+                        {props.props.phone || '123-456-789'}
+                    </p>
+                </div>
+
+                <div className="cv-contact-div">    
+                    <p className='cv-contact-p'>
+                        {props.props.address || 'City, State'}
+                    </p>
+                </div>
+
+                
+                <div className="cv-contact-div">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="1em" height="1em" viewBox="0 0 50 50" className='fill-blue'>
+                        <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
+                    </svg>
+                    
+                    <p className='cv-contact-p'>
+                        {props.props.linkedin || 'linkedin.com/your-profile'}
+                    </p>
+                </div>
+
+                <div className="cv-contact-div">
+                    <p className='cv-contact-p'>
+                        {props.props.website || 'github.com/your-profile'}
+                    </p>
+                </div>
             </div>
         </>
     )
