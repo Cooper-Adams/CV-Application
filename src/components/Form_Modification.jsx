a7327a34b1efa6bab0cf13735bdae79ada0041fb
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-21 18:47:58 -0500

    Removed the 'load form example' portion

--- src/components/Form_Modification.jsx
+++ src/components/Form_Modification.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 const Form_Modification = (props) => {
   const clearForm = () => { props.wipeFormData() }
-  const loadForm = () => { props.exampleFormData() }
   const downloadCV = () => { props.generatePDF() }
 
   return (
     <>
       <div className='form-mod'>
         <div className='modification-btn' onClick={clearForm}><span>Clear CV</span></div>
-        <div className='modification-btn' onClick={loadForm}><span>Load Example</span></div>
         <div className='modification-btn' onClick={downloadCV}><span>Download</span></div>
       </div>
     </>
