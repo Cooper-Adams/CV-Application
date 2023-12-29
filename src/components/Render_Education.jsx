4182253a0f583c62fa064692c6dd79e3c31f0b60
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-29 00:45:15 -0500

    Removed object prop notation, will be replaced with a map of the prop args

--- src/components/Render_Education.jsx
+++ src/components/Render_Education.jsx
@@ -3,65 +3,7 @@ import React from 'react'
 const Render_Education = (props) => {
     return (
         <>
-            <div className='cv-name-desc'>
-                {Object.entries(props).map(([key, value]) => {
-                    if (key == 'fullName') {
-                        return (
-                            <h1 key={key} className='cv-title-h1'>
-                                {value}
-                            </h1>
-                        )
-                    } else if (key == 'title') {
-                        return (
-                            <h3 key={key} className='cv-title-h3'>
-                                {value}
-                            </h3>
-                        )
-                    } else if (key == 'description') {
-                        return (
-                            <p key={key} className='cv-desc-p'>
-                                {value}
-                            </p>
-                        )
-                    }
-                })}
-            </div>
-
-            <div className='cv-contacts'>
-                {Object.entries(props).map(([key, value]) => {
-                    if (key == 'address') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {value}
-                            </p>
-                        )
-                    } else if (key == 'email') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {value}
-                            </p>
-                        )
-                    } else if (key == 'phone') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {value}
-                            </p>
-                        )
-                    } else if (key == 'linkedin') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {value}
-                            </p>
-                        )
-                    } else if (key == 'website') {
-                        return (
-                            <p key={key} className='cv-contact-p'>
-                                {value}
-                            </p>
-                        )
-                    }
-                })}
-            </div>
+           <h5>EDUCATION</h5>
         </>
     )
 }
