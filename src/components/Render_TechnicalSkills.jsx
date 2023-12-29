5093c14a2ccc0dac760bfd1843e92176afa999fe
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-29 00:48:18 -0500

    Removed object prop notation, will be replaced with a map of the prop args

--- src/components/Render_TechnicalSkills.jsx
+++ src/components/Render_TechnicalSkills.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 const Render_TechnicalSkills = (props) => {
     return (
         <>
-            <h2>TECHNICAL (IT) SKILLS</h2>
-            <div className='cv-technical-skills'>
-                {Object.entries(props).map(([key, value]) => {
-                    <p key={key} className='cv-interests-text'>
-                        {value}
-                    </p>
-                })}
-            </div>
+            <h5>TECHNICAL SKILLS</h5>
         </>
     )
 }
