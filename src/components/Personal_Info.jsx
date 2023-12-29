e5bdcd2717d5f3d9ed0a7739f78207c47e26cc82
Cooper Adams <cooperadamsbusiness@gmail.com>
2023-12-29 00:44:28 -0500

    Reworked the personal info component so that the CV updates 1 to 1 with the text input

--- src/components/Personal_Info.jsx
+++ src/components/Personal_Info.jsx
@@ -1,53 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Personal_Info = (props) => {
-  const [fullName, setFullName] = useState('')
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-
-  const handleChange = (e) => {
-    switch (e.target.name) {
-      case 'fullName':
-        setFullName(e.target.value)
-        break
-      case 'title':
-        setTitle(e.target.value)
-        break
-      case 'description':
-        setDescription(e.target.value)
-        break
-    }
-
-    e.preventDefault()
-
-    handleFormSubmit(e)
-  }
-
-  const handleFormSubmit = (e) => {    
-    const formData = {
-      fullName: fullName,
-      title: title,
-      description: description,
-      form: 'personal-form',
-    }
-
-    props.saveFormInput(formData)
-  }
-
   return (
     <>
-      {console.log(fullName)}
-      <form id='personal-form'>
+      <form id='form personal-form'>
         <div className='info-div active'>
             <div className='info-head'>
               <h2 className='info-title'>Personal Information</h2>
             </div>
 
-            <input className='info-input' name='fullName' type='text' placeholder='Full Name' value={fullName} onChange={handleChange} required />
+            <input className='info-input' name='fullName' type='text' placeholder='Full Name' value={props.fullName} onChange={props.personalInfoChange} required />
 
-            <input className='info-input' name='title' type='text' placeholder='Personal Title' value={title} onChange={handleChange} required />
+            <input className='info-input' name='title' type='text' placeholder='Personal Title' value={props.title} onChange={props.personalInfoChange} required />
             
-            <textarea className='info-input' name='description' value={description} onChange={handleChange} cols='30' rows='10' placeholder='Personal Description'></textarea>
+            <textarea className='info-input' name='description' value={props.description} onChange={props.personalInfoChange} cols='30' rows='10' placeholder='Personal Description'></textarea>
         </div>
       </form>
     </>
