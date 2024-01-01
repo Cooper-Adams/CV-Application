9d2f6721a9b467eb1510019941acbc97d89c9870
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-01 01:08:30 -0500

    Fixed the render component to display a list of the differing technical skill lists

--- src/components/Render_TechnicalSkills.jsx
+++ src/components/Render_TechnicalSkills.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Render_TechnicalSkills = (props) => {
+    const techSkillRender = props.formData.map((skillCat) => (
+        <div className='fp-inner-div'>
+            <h5 className='fp-inner-header'>{skillCat.title}</h5>
+            <ul className='fp-inner-ul'>
+                {skillCat.additionalInfo.map((skill) => (
+                    <li className='fp-inner-li'>
+                        <p className='fp-inner-p'>{skill.content}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    ))
+
     return (
         <>
             <h5>TECHNICAL SKILLS</h5>
+
+            {techSkillRender}
         </>
     )
 }
