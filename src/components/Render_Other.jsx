90cb8814ca2e9a10e4b2a5e261bebc932bf886f9
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-01 01:07:28 -0500

    Added a baseline check to ensure there is data to be placed and not just an empty section

--- src/components/Render_Other.jsx
+++ src/components/Render_Other.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Render_Other = (props) => {
-    return (
-        <>
-        </>
-    )
+    if (props.formData.length == 0) {
+        return (<></>)
+    } else {
+        return (
+            <>
+                
+            </>
+        )
+    }
 }
 
 export default Render_Other
