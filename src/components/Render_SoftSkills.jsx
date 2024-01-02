d088043b57a71961065b7bd5e98375edcccf9ce7
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-02 01:16:52 -0500

    Added unique key to map children to fix error

--- src/components/Render_SoftSkills.jsx
+++ src/components/Render_SoftSkills.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const Render_SoftSkills = (props) => {
+    const softSkillRender = props.formData.map((skill) => (        
+        <span key={skill} className='fp-inner-span'>{skill.content}</span>
+    ))
+
     return (
         <>
             <h5>SOFT SKILLS</h5>
+
+            <div className='fp-inner-div fp-soft'>
+                {softSkillRender}
+            </div>
         </>
     )
 }
