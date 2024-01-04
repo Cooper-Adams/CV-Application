b9567a4eb845d0aa8fc219aead411d34c58dffaf
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-04 17:11:35 -0500

    Filled out the Render other section to have an unordered list of the items in the submitted category

--- src/components/Render_Other.jsx
+++ src/components/Render_Other.jsx
@@ -1,15 +1,28 @@
+import { nanoid } from 'nanoid'
 import React from 'react'
 
 const Render_Other = (props) => {
-    if (props.formData.length == 0) {
-        return (<></>)
-    } else {
+    const otherCatRender = props.formData.map(cat => {
         return (
-            <>
-                
-            </>
+            <div key={nanoid()} className='fp-inner-div'>
+                <h5>{cat.title.toUpperCase()}</h5>
+
+                <ul className='fp-inner-ul'>
+                    {cat.additionalInfo.map((skill) => (
+                        <li key={nanoid()} className='fp-inner-li'>
+                            <p className='fp-inner-p'>{skill.content}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         )
-    }
+    })
+
+    return (
+        <>
+            {otherCatRender}
+        </>
+    )
 }
 
 export default Render_Other
