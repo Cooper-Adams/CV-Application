bdac2a369ceab0bf5037fcf98e2456d2c4ce6af6
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-04 18:41:10 -0500

    Filled out the education and professional render components

--- src/components/Render_Education.jsx
+++ src/components/Render_Education.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
 const Render_Education = (props) => {
+    const eduRender = props.formData.map(edu => {
+        console.log(edu)
+        return(
+            <div className='fp-inner-container'>
+                <div className='fp-inner-upper'>
+                    <div className='fp-titles-div'>
+                        <h3 className='fp-inner-title-head'>{edu.schoolName}</h3>
+                        <h4 className='fp-inner-subtitle-head'>{edu.degree}</h4>
+                    </div>
+
+                    <span className='fp-inner-tofrom'>{edu.start} - {edu.finish}</span>
+                </div>
+
+                <ul className='fp-inner-lower-ul'>
+                    {edu.additionalInfo.map(task => (
+                        <li key={nanoid()} className='fp-inner-left-li'>
+                            <span className='fp-inner-span'>{task.content}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    })
+
     return (
         <>
-           <h5>EDUCATION</h5>
+            <h5>EDUCATION</h5>
+
+            <div className='fp-inner-div'>
+                {eduRender}
+            </div>
         </>
     )
 }
--- src/components/Render_Professional.jsx
+++ src/components/Render_Professional.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
 const Render_Professional = (props) => {
+    const proRender = props.formData.map(job => {
+        console.log(job)
+        return(
+            <div className='fp-inner-container'>
+                <div className='fp-inner-upper'>
+                    <div className='fp-titles-div'>
+                        <h3 className='fp-inner-title-head'>{job.jobTitle}</h3>
+                        <h4 className='fp-inner-subtitle-head'>{job.companyName}</h4>
+                    </div>
+
+                    <span className='fp-inner-tofrom'>{job.startDate} - {job.finishDate}</span>
+                </div>
+
+                <ul className='fp-inner-lower-ul'>
+                    {job.additionalInfo.map(task => (
+                        <li key={nanoid()} className='fp-inner-left-li'>
+                            <span className='fp-inner-span'>{task.content}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    })
+
     return (
         <>
             <h5>WORK EXPERIENCE</h5>
 
-
+            <div className='fp-inner-div'>
+                {proRender}
+            </div>
         </>
     )
 }
