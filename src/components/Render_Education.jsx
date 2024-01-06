79d1004241f6714f3715fc0a62ed40c397881d88
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-06 14:38:25 -0500

    Removed console.log and provided default values for if user leaves input blank

--- src/components/Render_Education.jsx
+++ src/components/Render_Education.jsx
@@ -3,22 +3,21 @@ import { nanoid } from 'nanoid'
 
 const Render_Education = (props) => {
     const eduRender = props.formData.map(edu => {
-        console.log(edu)
         return(
             <div className='fp-inner-container'>
                 <div className='fp-inner-upper'>
                     <div className='fp-titles-div'>
-                        <h3 className='fp-inner-title-head'>{edu.schoolName}</h3>
-                        <h4 className='fp-inner-subtitle-head'>{edu.degree}</h4>
+                        <h3 className='fp-inner-title-head'>{edu.schoolName || 'School Name'}</h3>
+                        <h4 className='fp-inner-subtitle-head'>{edu.degree || 'Degree Title'}</h4>
                     </div>
 
-                    <span className='fp-inner-tofrom'>{edu.start} - {edu.finish}</span>
+                    <span className='fp-inner-tofrom'>{edu.start || 'Start Date'} - {edu.finish || 'Finish Date'}</span>
                 </div>
 
                 <ul className='fp-inner-lower-ul'>
                     {edu.additionalInfo.map(task => (
                         <li key={nanoid()} className='fp-inner-left-li'>
-                            <span className='fp-inner-span'>{task.content}</span>
+                            <span className='fp-inner-span'>{task.content || 'Additional Information'}</span>
                         </li>
                     ))}
                 </ul>
--- src/components/Render_Professional.jsx
+++ src/components/Render_Professional.jsx
@@ -3,22 +3,21 @@ import { nanoid } from 'nanoid'
 
 const Render_Professional = (props) => {
     const proRender = props.formData.map(job => {
-        console.log(job)
         return(
             <div className='fp-inner-container'>
                 <div className='fp-inner-upper'>
                     <div className='fp-titles-div'>
-                        <h3 className='fp-inner-title-head'>{job.jobTitle}</h3>
-                        <h4 className='fp-inner-subtitle-head'>{job.companyName}</h4>
+                        <h3 className='fp-inner-title-head'>{job.jobTitle || 'Job Title'}</h3>
+                        <h4 className='fp-inner-subtitle-head'>{job.companyName || 'Company Name'}</h4>
                     </div>
 
-                    <span className='fp-inner-tofrom'>{job.startDate} - {job.finishDate}</span>
+                    <span className='fp-inner-tofrom'>{job.startDate || 'Start Date'} - {job.finishDate || 'Finish Date'}</span>
                 </div>
 
                 <ul className='fp-inner-lower-ul'>
                     {job.additionalInfo.map(task => (
                         <li key={nanoid()} className='fp-inner-left-li'>
-                            <span className='fp-inner-span'>{task.content}</span>
+                            <span className='fp-inner-span'>{task.content || 'Additional Information'}</span>
                         </li>
                     ))}
                 </ul>
