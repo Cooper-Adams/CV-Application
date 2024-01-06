e53eeb65345a77c0d9715629a389f869644ea5b1
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-06 15:36:08 -0500

    Added a container class to prevent spacing issues

--- src/components/Render_TechnicalSkills.jsx
+++ src/components/Render_TechnicalSkills.jsx
@@ -4,14 +4,16 @@ import { nanoid } from 'nanoid'
 const Render_TechnicalSkills = (props) => {
     const techSkillRender = props.formData.map((skillCat) => (
         <div key={nanoid()} className='fp-inner-div'>
-            <h5 className='fp-inner-header'>{skillCat.title}</h5>
-            <ul className='fp-inner-ul'>
-                {skillCat.additionalInfo.map((skill) => (
-                    <li key={nanoid()} className='fp-inner-li'>
-                        <p className='fp-inner-p'>{skill.content}</p>
-                    </li>
-                ))}
-            </ul>
+            <div className='fp-inner-container'>
+                <h4 className='fp-inner-header'>{skillCat.title}</h4>
+                <ul className='fp-inner-ul'>
+                    {skillCat.additionalInfo.map((skill) => (
+                        <li key={nanoid()} className='fp-inner-li'>
+                            <p className='fp-inner-p'>{skill.content}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     ))
 
