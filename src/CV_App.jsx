843f8b3fedc06c05be354f1bfb8d09e62f7d7c31
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-07 01:24:46 -0500

    Added a button that is fixed in the bottom right which downloads the CV when clicked

--- src/CV_App.jsx
+++ src/CV_App.jsx
@@ -150,7 +150,7 @@ function CV_App () {
         />
       </div>
 
-      <div className="right-panel">
+      <div className='right-panel'>
         <div className='cv-preview' id='cv-pdf'>
           <div className='cv-section personal'>
             <Render_Personal 
@@ -159,8 +159,8 @@ function CV_App () {
             />
           </div>
           
-          <div className="cv-section below-head">
-            <div className="cv-section bh-left">
+          <div className='cv-section below-head'>
+            <div className='cv-section bh-left'>
               <div className='cv-section bhs'>
                 <Render_Professional  formData = {formData.experienceInfo} />
               </div>
@@ -174,7 +174,7 @@ function CV_App () {
               </div>
             </div>
 
-            <div className="cv-section bh-right">
+            <div className='cv-section bh-right'>
               <div className='cv-section bhs'>
                 <Render_TechnicalSkills formData = {formData.technicalSkills} />
               </div>
@@ -190,6 +190,25 @@ function CV_App () {
           </div>
         </div>
       </div>
+        
+      <button class='dl-button' onClick={generatePDF}>
+        <svg
+          width='40'
+          height='40'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M11 5C11 4.44772 11.4477 4 12 4C12.5523 4 13 4.44772 13 5V12.1578L16.2428 8.91501L17.657 10.3292L12.0001 15.9861L6.34326 10.3292L7.75748 8.91501L11 12.1575V5Z'
+            fill='#003060'
+          />
+          <path
+            d='M4 14H6V18H18V14H20V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V14Z'
+            fill='#003060'
+          />
+        </svg>
+      </button>
     </>
   )
 }
