aeb13869c0383ff10c8a4e52efc0e9bd64606805
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-01-08 14:47:03 -0500

    Added an Edit button to the submitted work/edu items

--- src/components/Education_Info.jsx
+++ src/components/Education_Info.jsx
@@ -7,7 +7,7 @@ const Education_Info = (props) => {
     degree: '',
     start: '',
     finish: '',
-    extras: [],
+    additionalInfo: [],
     currentTask: ''
   })
 
@@ -16,7 +16,7 @@ const Education_Info = (props) => {
     degree: '',
     start: '',
     finish: '',
-    extras: [],
+    additionalInfo: [],
     currentTask: ''
   }
 
@@ -45,8 +45,8 @@ const Education_Info = (props) => {
 
     setEduInfo((prevInfo) => ({
       ...prevInfo,
-      extras: [
-        ...prevInfo.extras,
+      additionalInfo: [
+        ...prevInfo.additionalInfo,
         {
           id: nanoid(),
           content: infoContent,
@@ -55,6 +55,26 @@ const Education_Info = (props) => {
       currentTask: '',
     }))
   }
+
+  const deleteSkill = (e) => {
+    setEduInfo((prevInfo) => ({
+        ...prevInfo,
+        additionalInfo: prevInfo.additionalInfo.filter((task) => task.id !== e.target.id)
+    }))
+
+    if ((eduInfo.additionalInfo.length - 1) == 0) {
+        document.querySelector('.edu-container').classList.toggle('work-active')
+    }
+  }
+
+  const displaySkills = eduInfo.additionalInfo.map((task) => (
+    <li key={nanoid()} className='added-task'>
+        <span id={nanoid()} className='extra-info'>{task.content}</span>
+        <button type='button' id={task.id} onClick={deleteSkill}>
+        X
+        </button>
+    </li>
+  ))
 
   const submitEduInfo = (e) => {
     if (!document.querySelector('.edu-exp').classList.contains('work-active')) {
@@ -65,16 +85,28 @@ const Education_Info = (props) => {
 
     setEduInfo(blankState)
 
-    if (eduInfo.extras.length != 0) {
+    if (eduInfo.additionalInfo.length != 0) {
       document.querySelector('.edu-container').classList.toggle('work-active')
     }
   }
 
-  const deleteWorkInfo = (e) => {
+  const deleteEduInfo = (e) => {
     props.deleteOtherCat(e.target.id, 'educationInfo')
 
     if ((props.formData.length - 1) == 0) {
       document.querySelector('.edu-exp').classList.toggle('work-active')
+    }
+  }
+
+  const editEduInfo = (e) => {
+    let infoToEdit = props.formData.find((exp) => exp.id === e.target.id)
+
+    deleteEduInfo(e)
+
+    setEduInfo(infoToEdit)
+
+    if (infoToEdit.additionalInfo.length) {
+      document.querySelector('.edu-container').classList.toggle('work-active')
     }
   }
 
@@ -91,9 +123,47 @@ const Education_Info = (props) => {
               {props.formData.map(exp => (
                 <li key={nanoid()} className='added-work-exp'>
                   <span id={nanoid()} className='extra-info'>{exp.schoolName + ', ' + exp.start + ' - ' + exp.finish}</span>
-                  <button type='button' id={exp.id} onClick={deleteWorkInfo}>
-                  X
-                  </button>
+                  <div className='e-btns'>
+                    <button type='button' id={exp.id} onClick={editEduInfo}>
+                      <svg
+                        width='1.5em'
+                        height='1.5em'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M10 4H8V6H5C3.34315 6 2 7.34315 2 9V15C2 16.6569 3.34315 18 5 18H8V20H10V4ZM8 8V16H5C4.44772 16 4 15.5523 4 15V9C4 8.44772 4.44772 8 5 8H8Z'
+                          fill='currentColor'
+                        />
+                        <path
+                          d='M19 16H12V18H19C20.6569 18 22 16.6569 22 15V9C22 7.34315 20.6569 6 19 6H12V8H19C19.5523 8 20 8.44771 20 9V15C20 15.5523 19.5523 16 19 16Z'
+                          fill='currentColor'
+                        />
+                      </svg>
+                    </button>
+                    
+                    <button type='button' id={exp.id} onClick={deleteEduInfo}>
+                      <svg
+                        width='1.5em'
+                        height='1.5em'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z'
+                          fill='currentColor'
+                        />
+                        <path d='M9 9H11V17H9V9Z' fill='currentColor' />
+                        <path d='M13 9H15V17H13V9Z' fill='currentColor' />
+                      </svg>
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
@@ -115,20 +185,7 @@ const Education_Info = (props) => {
 
           <div className='edu-container'>
             <ul className='ul-skill'>
-              {eduInfo.extras.map(skills => (
-                <li key={nanoid()} className='added-task'>
-                  <span id={nanoid()} className='extra-info'>{skills.content}</span>
-                  <button onClick={() => {
-                    setEduInfo(
-                      eduInfo.extras.filter(a =>
-                        a.id !== skills.id
-                      )
-                    )
-                  }}>
-                  X
-                  </button>
-                </li>
-              ))}
+              {eduInfo.additionalInfo.length ? displaySkills : ''}
             </ul>
           </div>
 
--- src/components/Work_Info.jsx
+++ src/components/Work_Info.jsx
@@ -7,7 +7,7 @@ const Work_Info = (props) => {
     jobTitle: '',
     startDate: '',
     finishDate: '',
-    tasks: [],
+    additionalInfo: [],
     currentTask: ''
   })
 
@@ -16,7 +16,7 @@ const Work_Info = (props) => {
     jobTitle: '',
     startDate: '',
     finishDate: '',
-    tasks: [],
+    additionalInfo: [],
     currentTask: ''
   }
 
@@ -45,8 +45,8 @@ const Work_Info = (props) => {
 
     setWorkInfo((prevInfo) => ({
       ...prevInfo,
-      tasks: [
-        ...prevInfo.tasks,
+      additionalInfo: [
+        ...prevInfo.additionalInfo,
         {
           id: nanoid(),
           content: infoContent,
@@ -55,6 +55,26 @@ const Work_Info = (props) => {
       currentTask: '',
     }))
   }
+
+  const deleteSkill = (e) => {
+    setWorkInfo((prevInfo) => ({
+        ...prevInfo,
+        additionalInfo: prevInfo.additionalInfo.filter((task) => task.id !== e.target.id)
+    }))
+
+    if ((workInfo.additionalInfo.length - 1) == 0) {
+        document.querySelector('.skill-container').classList.toggle('work-active')
+    }
+  }
+  
+  const displaySkills = workInfo.additionalInfo.map((task) => (
+    <li key={nanoid()} className='added-task'>
+        <span id={nanoid()} className='extra-info'>{task.content}</span>
+        <button type='button' id={task.id} onClick={deleteSkill}>
+        X
+        </button>
+    </li>
+  ))
 
   const submitWorkInfo = (e) => {
     if (!document.querySelector('.work-exp').classList.contains('work-active')) {
@@ -65,7 +85,7 @@ const Work_Info = (props) => {
 
     setWorkInfo(blankState)
 
-    if (workInfo.tasks.length != 0) {
+    if (workInfo.additionalInfo.length != 0) {
       document.querySelector('.skill-container').classList.toggle('work-active')
     }
   }
@@ -75,6 +95,18 @@ const Work_Info = (props) => {
 
     if ((props.formData.length - 1) == 0) {
       document.querySelector('.work-exp').classList.toggle('work-active')
+    }
+  }
+
+  const editWorkInfo = (e) => {
+    let infoToEdit = props.formData.find((exp) => exp.id === e.target.id)
+
+    deleteWorkInfo(e)
+
+    setWorkInfo(infoToEdit)
+
+    if (infoToEdit.additionalInfo.length) {
+      document.querySelector('.skill-container').classList.toggle('work-active')
     }
   }
 
@@ -91,9 +123,47 @@ const Work_Info = (props) => {
               {props.formData.map(exp => (
                 <li key={nanoid()} className='added-work-exp'>
                   <span id={nanoid()} className='extra-info'>{exp.companyName + ', ' + exp.startDate + ' - ' + exp.finishDate}</span>
-                  <button type='button' id={exp.id} onClick={deleteWorkInfo}>
-                  X
-                  </button>
+                  <div className='e-btns'>
+                    <button type='button' id={exp.id} onClick={editWorkInfo}>
+                      <svg
+                        width='1.5em'
+                        height='1.5em'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M10 4H8V6H5C3.34315 6 2 7.34315 2 9V15C2 16.6569 3.34315 18 5 18H8V20H10V4ZM8 8V16H5C4.44772 16 4 15.5523 4 15V9C4 8.44772 4.44772 8 5 8H8Z'
+                          fill='currentColor'
+                        />
+                        <path
+                          d='M19 16H12V18H19C20.6569 18 22 16.6569 22 15V9C22 7.34315 20.6569 6 19 6H12V8H19C19.5523 8 20 8.44771 20 9V15C20 15.5523 19.5523 16 19 16Z'
+                          fill='currentColor'
+                        />
+                      </svg>
+                    </button>
+                    
+                    <button type='button' id={exp.id} onClick={deleteWorkInfo}>
+                      <svg
+                        width='1.5em'
+                        height='1.5em'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z'
+                          fill='currentColor'
+                        />
+                        <path d='M9 9H11V17H9V9Z' fill='currentColor' />
+                        <path d='M13 9H15V17H13V9Z' fill='currentColor' />
+                      </svg>
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
@@ -115,20 +185,7 @@ const Work_Info = (props) => {
 
           <div className='skill-container'>
             <ul className='ul-skill'>
-              {workInfo.tasks.map(skills => (
-                <li key={nanoid()} className='added-task'>
-                  <span id={nanoid()} className='extra-info'>{skills.content}</span>
-                  <button onClick={() => {
-                    setWorkInfo(
-                      workInfo.tasks.filter(a =>
-                        a.id !== skills.id
-                      )
-                    )
-                  }}>
-                  X
-                  </button>
-                </li>
-              ))}
+              {workInfo.additionalInfo.length ? displaySkills : ''}
             </ul>
           </div>
 
