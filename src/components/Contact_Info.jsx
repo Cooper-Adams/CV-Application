4bd654ff6e6981b74d64ed64ba422e51c638124c
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-08-26 02:12:46 -0400

    Updated inputs to contain autocomplete attributes on common fields

--- src/components/Contact_Info.jsx
+++ src/components/Contact_Info.jsx
@@ -9,11 +9,11 @@ const Contact_Info = (props) => {
                 <h2 className='info-title'>Contact Information</h2>
             </div>
         
-            <input className='info-input' name='email' type='email' placeholder='Email Address' value={props.formData.email} onChange={props.contactInfoChange} required />
+            <input className='info-input' autoComplete='true' name='email' type='email' placeholder='Email Address' value={props.formData.email} onChange={props.contactInfoChange} required />
             
-            <input className='info-input' name='phone' type='tel' placeholder='Phone Number' value={props.formData.phone} onChange={props.contactInfoChange} required />
+            <input className='info-input' autoComplete='true' name='phone' type='tel' placeholder='Phone Number' value={props.formData.phone} onChange={props.contactInfoChange} required />
 
-            <input className='info-input' name='address' type='text' placeholder='City, State' value={props.formData.address} onChange={props.contactInfoChange} required />            
+            <input className='info-input' autoComplete='true' name='address' type='text' placeholder='City, State' value={props.formData.address} onChange={props.contactInfoChange} required />            
             
             <input className='info-input' name='linkedin' type='text' placeholder='LinkedIn' value={props.formData.linkedin} onChange={props.contactInfoChange} required />
             
