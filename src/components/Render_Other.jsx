5df7109c0d6952bb4cbf575f5068cfeec9f419b5
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-08-26 20:33:46 -0400

    Fixed key issue

--- src/components/Render_Other.jsx
+++ src/components/Render_Other.jsx
@@ -2,21 +2,21 @@ import { nanoid } from 'nanoid'
 import React from 'react'
 
 const Render_Other = (props) => {
-    const otherCatRender = props.formData.map(cat => {
-        return ( <>
+    const otherCatRender = props.formData.map((cat) => (
+        <div key={nanoid()}>
             <h5 key={nanoid()}>{cat.title.toUpperCase()}</h5>
             
             <div key={nanoid()} className='fp-inner-div'>
-                <ul className='fp-inner-ul'>
+                <ul key={nanoid()} className='fp-inner-ul'>
                     {cat.additionalInfo.map((skill) => (
                         <li key={nanoid()} className='fp-inner-li'>
-                            <p className='fp-inner-p'>{skill.content}</p>
+                            <p key={nanoid()} className='fp-inner-p'>{skill.content}</p>
                         </li>
                     ))}
                 </ul>
             </div>
-        </>)
-    })
+        </div>
+    ))
 
     return (
         <>
