114448a6c5c3bf9e8e2b48ad3db497ee4c9043cd
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-08-26 20:33:29 -0400

    Changed to allow users to edit other submitted info

--- src/components/Other_Info.jsx
+++ src/components/Other_Info.jsx
@@ -5,14 +5,15 @@ import { nanoid } from 'nanoid'
 const Other_Info = (props) => {
   const [otherCat, setOtherCat] = useState({
     title: '',
-    skills: [],
+    additionalInfo: [],
     currentTask: '',
-    side: false
+    side: false,
+    id: ''
   })
 
   const blankState = {
     title: '',
-    skills: [],
+    additionalInfo: [],
     currentTask: '',
     side: false
   }
@@ -34,12 +35,12 @@ const Other_Info = (props) => {
   }
 
   const submitTask = (e) => {
-    let infoContent;
+    let infoContent
 
-    if (e.type === 'keydown' && e.key !== 'Enter') return;
+    if (e.type === 'keydown' && e.key !== 'Enter') return
     if (e.type === 'click') {
-      infoContent = e.target.previousElementSibling.value;
-    } else if (e.key === 'Enter') infoContent = e.target.value;
+      infoContent = e.target.previousElementSibling.value
+    } else if (e.key === 'Enter') infoContent = e.target.value
 
     if (!infoContent) { return }
 
@@ -49,8 +50,8 @@ const Other_Info = (props) => {
 
     setOtherCat((prevInfo) => ({
       ...prevInfo,
-      skills: [
-        ...prevInfo.skills,
+      additionalInfo: [
+        ...prevInfo.additionalInfo,
         {
           id: nanoid(),
           content: infoContent,
@@ -63,17 +64,17 @@ const Other_Info = (props) => {
   const deleteSkill = (e) => {
     setOtherCat((prevInfo) => ({
         ...prevInfo,
-        skills: prevInfo.skills.filter((skill) => skill.id !== e.target.id)
+        additionalInfo: prevInfo.additionalInfo.filter((skill) => skill.id !== e.target.id)
     }))
 
-    if ((otherCat.skills.length - 1) == 0) {
+    if ((otherCat.additionalInfo.length - 1) == 0) {
         document.querySelector('.other-container').classList.toggle('work-active')
     }
   }
   
-  const displaySkills = otherCat.skills.map((skill) => (
+  const displaySkills = otherCat.additionalInfo.map((skill) => (    
     <li key={nanoid()} className='added-task'>
-        <span id={nanoid()} className='extra-info'>{skill.content}</span>
+        <span id={nanoid()} className='extra-info ei-task'>{skill.content}</span>
         <button type='button' id={skill.id} onClick={deleteSkill}>
         X
         </button>
@@ -89,7 +90,7 @@ const Other_Info = (props) => {
 
     setOtherCat(blankState)
 
-    if (otherCat.skills.length != 0) {
+    if (otherCat.additionalInfo.length != 0) {
       document.querySelector('.other-container').classList.toggle('work-active')
     }
   }
@@ -106,22 +107,85 @@ const Other_Info = (props) => {
     }
   }
 
+  const editOtherInfo = (e) => {
+    let infoToEdit = props.formData.find((exp) => exp.id === e.target.id)
+
+    setOtherCat(infoToEdit)
+
+    if (infoToEdit.additionalInfo.length) {
+      document.querySelector('.other-container').classList.toggle('work-active')
+    }
+  }
+
+  const reOrder = (e) => { 
+    props.reorderInfo(e, 'educationInfo')
+  }
+
+  console.log(otherCat.title)
+
   return (
     <>
       <form id='form other-form'>
         <div className='info-div'>
           <Section_Head section_title={'Other Information'}/>
           
-          <span>If you have extra space and still have relevant information to display, add it here.</span>
+          <span style={{display: (props.formData.length > 0) ? 'none' : 'block'}}>If you have extra space and still have relevant information to display, add it here.</span>
 
           <div className='other-exp'>
             <ul className='ul-total'>
-              {props.formData.map(exp => (
+              {props.formData.map((exp, index) => (
                 <li key={nanoid()} className='added-work-exp'>
                     <span id={nanoid()}>{exp.title}</span>
-                    <button type='button' id={exp.id} value={exp.side} onClick={deleteSkillInfo}>
-                    X
-                    </button>
+
+                    <div className='e-btns'>
+                      <button type='button' id={index} onClick={reOrder} style={{display: (props.formData.length == 1 || index == 0) ? 'none' : 'block'}}>
+                        <svg width='1.5em' height='1.5em' viewBox='0 0 1024 1024' className='icon' version='1.1' xmlns='http://www.w3.org/2000/svg' fill='none' transform='matrix(1, 0, 0, -1, 0, 0)'><g id='SVGRepo_bgCarrier' strokeWidth='0'></g><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g><g id='SVGRepo_iconCarrier'><path d='M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z' fill='currentColor'></path></g></svg>
+                      </button>
+
+                      <button type='button' className='down' id={index} onClick={reOrder} style={{display: (props.formData.length == 1 || index == props.formData.length - 1) ? 'none' : 'block'}}>
+                        <svg width='1.5em' height='1.5em' viewBox='0 0 1024 1024' className='icon' version='1.1' xmlns='http://www.w3.org/2000/svg' fill='none'><g id='SVGRepo_bgCarrier' strokeWidth='0'></g><g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g><g id='SVGRepo_iconCarrier'><path d='M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z' fill='currentColor'></path></g></svg>
+                      </button>
+
+                      <button type='button' id={exp.id} onClick={editOtherInfo}>
+                        <svg
+                          width='1.5em'
+                          height='1.5em'
+                          viewBox='0 0 24 24'
+                          fill='none'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            fillRule='evenodd'
+                            clipRule='evenodd'
+                            d='M10 4H8V6H5C3.34315 6 2 7.34315 2 9V15C2 16.6569 3.34315 18 5 18H8V20H10V4ZM8 8V16H5C4.44772 16 4 15.5523 4 15V9C4 8.44772 4.44772 8 5 8H8Z'
+                            fill='currentColor'
+                          />
+                          <path
+                            d='M19 16H12V18H19C20.6569 18 22 16.6569 22 15V9C22 7.34315 20.6569 6 19 6H12V8H19C19.5523 8 20 8.44771 20 9V15C20 15.5523 19.5523 16 19 16Z'
+                            fill='currentColor'
+                          />
+                        </svg>
+                      </button>
+
+                      <button type='button' id={exp.id} value={exp.side} onClick={deleteSkillInfo}>
+                        <svg
+                          width='1.5em'
+                          height='1.5em'
+                          viewBox='0 0 24 24'
+                          fill='none'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            fillRule='evenodd'
+                            clipRule='evenodd'
+                            d='M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z'
+                            fill='currentColor'
+                          />
+                          <path d='M9 9H11V17H9V9Z' fill='currentColor' />
+                          <path d='M13 9H15V17H13V9Z' fill='currentColor' />
+                        </svg>
+                      </button>
+                    </div>
                 </li>
               ))}
             </ul>
@@ -137,16 +201,18 @@ const Other_Info = (props) => {
 
           <span className='lrCheck'>
             Left Panel
-            <input className='lrSwitch' type='checkbox' name='side' id='lrSwitch' onChange={handleChange} value={otherCat.side} checked={otherCat.side}/>
+            <input className='lrSwitch' type='checkbox' name='side' id='lrSwitch' onChange={handleChange} value={otherCat.side} checked={JSON.parse(otherCat.side)}/>
             <label onClick={handleChange}></label>
             Right Panel
           </span>
 
           <div className='other-container'>
             <ul className='ul-skill'>
-                {otherCat.skills.length ? displaySkills : ''}
+                {otherCat.additionalInfo.length != 0 ? displaySkills : ''}
             </ul>
           </div>
+
+          <input className='info-input' name='id' type='text' defaultValue={otherCat.id} placeholder='id' hidden />
 
           <button type='button' className='submit-btn' onClick={submitSkillInfo}>Add Category</button>
         </div>
